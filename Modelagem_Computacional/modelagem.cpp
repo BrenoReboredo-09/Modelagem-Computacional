ca5419dcf260af3cbdab9cc513c2bd8bbf4d76be
BrenoReboredo-09 <127802083+BrenoReboredo-09@users.noreply.github.com>
2024-11-30 21:38:50 -0300

    Update modelagem.cpp

--- Modelagem_Computacional/modelagem.cpp
+++ Modelagem_Computacional/modelagem.cpp
@@ -12,7 +12,6 @@ double const k = 4500;
 double const m = 2000;
 double const pi = 4*atan(1);
 
-//función normmax
 double normmax(matrix<double> p){
     int n = p.rowno();
     double sum = 0;
@@ -21,7 +20,6 @@ double normmax(matrix<double> p){
     }
 return sum;
 }
-// función que devuelve los indices del mayor elemento fuera de la diagonal
 
 matrix<double> ind(matrix<double> A){
 double max = 0;
@@ -49,7 +47,6 @@ ind(0,0) = im;
 ind(0,1) = jm;
 return ind;
 }
-// función que diagonaliza
 void Adiag( matrix<double> (*ind)(matrix<double>), matrix<double> A, matrix<double>& Ad,int& nit, double tol, matrix<double>& U){
 
 int im = 0, jm = 0;
@@ -57,7 +54,6 @@ double theta = 0;
 int it = 0;
 matrix<double> c(1,2);c.null();
 U.unit();
-
 
 do{
   
@@ -91,26 +87,19 @@ A(im,jm) = A(jm,im);
  Ad = A;
 nit = it;
 
-
-
 }
 
 int main(){
     int n = 4;
-//creamos el fichero de salida
 string infile = "Kr.txt";
     ofstream ff(infile);
 
     if (ff.is_open()){
-
-
-    
+  
     matrix<double> Kr(4,4);Kr.null();
     matrix<double> K(4,4);K.null();
     matrix<double> M(4,4);M.null();
 
-
-// defino K y M
 for(int i=0;i<n;i++){
     for(int j=0;j<n;j++){
 
@@ -126,17 +115,11 @@ for(int i=0;i<n;i++){
 }
 K(n-1,n-1) = k;
 
-
  Kr = !(M) * K;
  cout<<"A matriz Kr é: "<<endl<<Kr<<endl;
 
 ff<< 4<<" "<<4<<endl;
         ff<< Kr; 
-
-
-
-
-
 ff.close();
 
 matrix<double> Ad(4,4);Ad.unit();
@@ -149,7 +132,6 @@ cout<<"A matriz diagonalizada é: "<<endl<< Ad<<endl;
 cout<< "A matriz de autovetor é: "<<endl<<U<<endl;
 cout<< "O número de iterações é: "<<nit<<endl;
 
-//prueba
 double err =0;
 err = normmax(~U*Kr*U- Ad);
 cout<<"O error é: "<<err<<endl;
